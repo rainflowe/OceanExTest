1146f5f6dd4c20159890a5cbce0a41b0765e12fc
rainflowe <187353113@qq.com>
2018-12-01 18:09:13 +0800

    ttt

--- TDEngineOceanEx.cpp
+++ TDEngineOceanEx.cpp
@@ -1754,3 +1754,15 @@ BOOST_PYTHON_MODULE(liboceanextd)
             .def("logout", &TDEngineOceanEx::logout)
             .def("wait_for_stop", &TDEngineOceanEx::wait_for_stop);
 }
+
+BOOST_PYTHON_MODULE(liboceanextd2)
+{
+    using namespace boost::python;
+    class_<TDEngineOceanEx, boost::shared_ptr<TDEngineOceanEx> >("Engine")
+            .def(init<>())
+            .def("init", &TDEngineOceanEx::initialize)
+            .def("start", &TDEngineOceanEx::start)
+            .def("stop", &TDEngineOceanEx::stop)
+            .def("logout", &TDEngineOceanEx::logout)
+            .def("wait_for_stop", &TDEngineOceanEx::wait_for_stop);
+}
